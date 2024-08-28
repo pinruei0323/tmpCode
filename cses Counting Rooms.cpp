24e4622000ed555307b355b0ac19474c7f07b924
pinruei0323 <yinlin980323@gmail.com>
2024-08-28 09:50:43 +0800

    Update

---
+++ cses Counting Rooms.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define f2(i, m) for(long long i=0; i<m; i++)
+#define f3(i, n, m) for(long long i=n; i<m; i++)
+#define ll long long
+#define pb push_back
+#define pob pop_back
+#define len length()
+#define elif else if
+#define mod 1000000007
+#define debug
+/*
+#ifdef debug
+#endif
+#ifndef debug
+#endif
+*/
+using namespace std;
+
+int main()
+{
+    ll n, m;
+    vector
+}
