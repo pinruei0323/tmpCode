ea338feecb280b41251d64e814c186a0c0f39e7d
pinruei0323 <yinlin980323@gmail.com>
2024-08-28 19:02:57 +0800

    11

--- cses Counting Rooms.cpp
+++ cses Counting Rooms.cpp
@@ -16,24 +16,28 @@
 */
 using namespace std;
 
-void dfs(ll a, ll b, auto &data, auto &visited, ll &ans)
+void dfs(ll a, ll b, auto &data, auto &visited, ll &ans, bool first, ll &n, ll &m)
 {
-    if(visited[a][b]==0 && data[a][b]=='.')
+    if(!visited[a][b] && data[a][b]=='.' && !(a<0 || b<0 || a>=n || b>=m))
     {
-        
+        visited[a][b]=1;
+        if(first)    ans++;
+        dfs(a+1, b, data, visited, ans, 0, n, m);
+        dfs(a-1, b, data, visited, ans, 0, n, m);
+        dfs(b+1, b, data, visited, ans, 0, n, m);
+        dfs(b-1, b, data, visited, ans, 0, n, m);
     }
-    
+    visited[a][b]=1;
 }
 
 int main()
 {
     ll n, m, ans=0;
-    vector <vector <char>> data(n, m);
+    vector <vector <char>> data(n, vector <char> (m));
     vector <vector <bool>> visited(n, vector <bool> (m));
     f2(i, n)
         f2(j, m)    cin >> data[i][j];
     f2(i, n)
-    {
-        f2(j, m)    dfs(0, 0, data, visited, ans);
+        f2(j, m)    dfs(0, 0, data, visited, ans, 1, n, m);
     cout << ans;
 }
